aee0222a58335082d50ceea6970d89a75df4e9c0
longee-gao <longee@163.com>
2023-10-18 18:33:33 +0800

    Core/Sql: bug fixed
    fixed sql update crash

--- sql/ashamane/characters/2018_03_11_01_characters.sql
+++ sql/ashamane/characters/2018_03_11_01_characters.sql
@@ -1,1 +1,2 @@
+﻿ALTER TABLE `item_instance_artifact` DROP COLUMN `artifactTierId`;
 ALTER TABLE `item_instance_artifact` ADD COLUMN `artifactTierId` INT(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `artifactAppearanceId`;
